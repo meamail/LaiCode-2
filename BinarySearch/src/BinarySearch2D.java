7116d964e58a43cabe7e958d5e6a2d073b9105be
Mia Wenjin Zhang <wenjinz@andrew.cmu.edu>
2019-01-03 12:13:56 +1030

    add classical solution to binary search in 2D matrix

--- BinarySearch/src/BinarySearch2D.java
+++ BinarySearch/src/BinarySearch2D.java
@@ -5,7 +5,8 @@ public class BinarySearch2D {
 
 		int[][] matrix = new int[][] { { 1, 4, 6, 8 }, { 10, 13, 15, 17 }, { 20, 22, 22, 24 } };
 		BinarySearch2D bs = new BinarySearch2D();
-		System.out.print("{" + bs.searchMatrix(matrix, 22)[0] + ", " + bs.searchMatrix(matrix, 22)[1] + "}");
+		System.out.println("{" + bs.searchMatrix(matrix, 22)[0] + ", " + bs.searchMatrix(matrix, 22)[1] + "}");
+		System.out.println("{" + bs.searchMatrix2(matrix, 22)[0] + ", " + bs.searchMatrix2(matrix, 22)[1] + "}");
 
 	}
 
@@ -41,5 +42,43 @@ public class BinarySearch2D {
 		// return the coordinate or -1,-1 of target not in matrix
 		return arr[left / m][left % m] == target ? new int[] { left / m, left % m } : new int[] { -1, -1 };
 	}
+
+	// binary search row, and then binary search column
+	// Time/Space O(log m*n)/ O(1)
+	public int[] searchMatrix2(int[][] matrix, int target) {
+
+		//corner case
+		if(matrix[0].length == 0) return new int[]{-1,-1};
+
+		int m = matrix.length;
+		int n = matrix[0].length;
+
+		int left = 0;
+		int right = m-1;
+
+		// search row
+		while (left < right - 1){
+			int mid = left + (right- left)/2;
+			if(matrix[mid][0] == target) return new int[]{mid, 0};
+			if(matrix[mid][0] < target) left = mid;
+			else right = mid - 1;
+
+		}
+
+		int row = matrix[right][0] <= target? right: left;// must <=
+
+		// search column
+		left = 0;
+		right = n-1;
+		while(left <=right){
+			int mid = left + (right- left)/2;
+			if(matrix[row][mid]== target) return new int[]{row, mid};
+			if(matrix[row][mid] < target) left = mid +1;
+			else right = mid -1;
+		}
+		return new int []{-1,-1};
+
+	}
+
 
 }
